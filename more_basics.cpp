9a0be75896e211882399197a43a67c13c694c075
Rahul Aloth Rajan <rahulalothrajan@gmail.com>
2025-02-11 09:18:38 +0100

    Update more_basics.cpp
    extern "C" example added.

--- more_basics.cpp
+++ more_basics.cpp
@@ -1,3 +1,17 @@
+#include <cstdio>
+#include <iostream>
+
+// climits -> defines constants that specify the limits of fundamental integral types 
+// for the specific system and compiler implementation used.Likewise cfloat for floating point.
+#include <climits>
+#include <cfloat>
+#include <vector>
+#include <assert.h>
+#include <map>
+#include <initializer_list>
+using namespace std;
+
+
 /*
 function overloading is same as that of old version of c++
 */
@@ -17,3 +31,18 @@ int function_overloading() {
 
 }
 
+/*
+  In C++, the extern "C" keyword is used to tell the C++ compiler to use
+  C linkage for the specified function(s). This is particularly useful when 
+  you need to call C functions from C++ code or when you want to ensure that your 
+  C++ functions can be called from C code
+*/
+
+// C++ code
+extern "C" {
+    void myCFunction(int, int);
+}
+
+void myCFunction(int x, int y) {
+    cout << "Sum of int's: " << add(x, y) << endl; // Calls double version   
+}
